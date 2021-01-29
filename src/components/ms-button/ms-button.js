48deb74d9927c1e6197e253cd235066f1db9be0f
Huixies <1062240360@qq.com>
2021-01-29 17:37:19 +0800

    将css文件模块化

---
+++ src/components/ms-button/ms-button.js
@@ -0,0 +1,20 @@
+import './ms-button.css';
+
+class MsButton{
+    render() {
+        const button = document.createElement('button');
+        button.innerHTML = 'MSButton';
+        button.classList.add('ms-button');
+        const body = document.querySelector('body');
+
+        body.onclick = function () {
+            const p = document.createElement('p');
+            p.innerHTML = '写代码';
+            p.classList.add('ms-text')
+            body.appendChild(p)
+        }
+        body.appendChild(button)
+    }
+}
+
+export default MsButton;
