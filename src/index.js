343128e958594d6b952456915ce3997b4c8ec0dd
Huixies <1062240360@qq.com>
2021-01-30 15:00:14 +0800

    删除目标路径下的文件

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import MsButton from './components/ms-button/ms-button.js';
+import MsHeading from './components/ms-heading/heading';
 
+const msHeading = new MsHeading();
+msHeading.render()
 const msButton = new MsButton();
 msButton.render()
