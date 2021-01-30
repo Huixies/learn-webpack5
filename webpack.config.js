c6c9e272540696e3bcfa3e7efeb496e9d61b1915
Huixies <1062240360@qq.com>
2021-01-30 08:38:05 +0800

    将scss模块化

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,13 @@ module.exports = {
                 use: [
                     'style-loader','css-loader'
                 ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     }
