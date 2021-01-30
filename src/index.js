fbfdaa3241f2e738b35c0308fb36aa011cc37947
Huixies <1062240360@qq.com>
2021-01-30 15:55:50 +0800

    配置开发和生产环境

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ msHeading.render()
 const msButton = new MsButton();
 msButton.render()
 
+if (process.env.NODE_ENV === 'production') {
+    console.log('Production mode');
+} else if (process.env.NODE_ENV === 'development') {
+    console.log('Development mode');
+}
+// msButton.justWannaDebugger();
